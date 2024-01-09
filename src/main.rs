63421964e89f8e0bfa7fb2175508f01b5feae875
Dariusz Depta <dariusz@confio.gmbh>
2024-01-09 17:53:31 +0100

    Added simple report.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod model;
+
+use crate::model::{report, Chain};
 use std::collections::BTreeMap;
 use std::fs;
 use std::path::Path;
@@ -14,18 +17,24 @@ fn search_files(path: &Path, files: &mut BTreeMap<String, DataFiles>) {
             if path.is_dir() {
                 search_files(&path, files);
             } else {
-                let parent_dir = path.parent().unwrap().to_string_lossy();
+                let parent_dir = path
+                    .canonicalize()
+                    .unwrap()
+                    .parent()
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string();
                 let file_name = path.file_name().unwrap().to_string_lossy();
                 if file_name == "chain.json" {
                     files
-                        .entry(parent_dir.to_string())
-                        .and_modify(|data| (*data).0 = Some(file_name.to_string()))
+                        .entry(parent_dir.clone())
+                        .and_modify(|data| data.0 = Some(file_name.to_string()))
                         .or_insert((Some(file_name.to_string()), None));
                 }
                 if file_name == "assetlist.json" {
                     files
-                        .entry(parent_dir.to_string())
-                        .and_modify(|data| (*data).1 = Some(file_name.to_string()))
+                        .entry(parent_dir.clone())
+                        .and_modify(|data| data.1 = Some(file_name.to_string()))
                         .or_insert((None, Some(file_name.to_string())));
                 }
             }
@@ -36,7 +45,12 @@ fn search_files(path: &Path, files: &mut BTreeMap<String, DataFiles>) {
 fn main() {
     let mut data_files = BTreeMap::new();
     search_files(Path::new(CHAIN_REGISTRY_REPO), &mut data_files);
-    for (a, b) in data_files {
-        println!("{} {:?}", a, b);
+    for (dir, (chain, _asset_list)) in data_files {
+        if let Some(chain) = chain {
+            let content = fs::read_to_string(format!("{}/{}", dir, chain))
+                .expect("loading chain.json failed");
+            let chain: Chain = serde_json::from_str(&content).expect("parsing chain.json failed");
+            println!("{:80} {}", dir, report(&chain));
+        }
     }
 }
